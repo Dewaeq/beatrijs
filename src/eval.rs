b88437ac94931b865738362768f6765a6c9e577f
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2023-03-24 19:48:54 +0100

    use tapered eval with pesto function

--- src/eval.rs
+++ src/eval.rs
@@ -2,20 +2,42 @@ use crate::{
     bitboard::BitBoard,
     board::Board,
     defs::{PieceType, Player, Score, Value},
-    gen::pesto::MG_TABLE,
+    gen::pesto::{EG_TABLE, MG_TABLE},
 };
 
+const GAME_PHASE_INC: [Score; 6] = [0, 1, 1, 2, 4, 0];
+
+/// see https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
 pub fn evaluate(board: &Board) -> Score {
-    let mut white = 0;
-    let mut black = 0;
+    let mut mg = [0; 2];
+    let mut eg = [0; 2];
+    let mut game_phase = 0;
 
-    count_psqt(board, &mut white, &mut black);
+    let mut sq = 0;
+    for piece in board.pieces {
+        if piece.is_none() {
+            sq += 1;
+            continue;
+        }
 
-    if board.turn == Player::White {
-        white - black
-    } else {
-        black - white
+        let idx = piece.c.as_usize();
+        mg[idx] += MG_TABLE[piece.as_usize()][sq];
+        eg[idx] += EG_TABLE[piece.as_usize()][sq];
+        game_phase += GAME_PHASE_INC[piece.t.as_usize()];
+
+        sq += 1;
     }
+
+    // tapered eval
+    let turn = board.turn.as_usize();
+    let opp = 1 - turn;
+
+    let mg_score = mg[turn] - mg[opp];
+    let eg_score = eg[turn] - eg[opp];
+    let mg_phase = Score::min(24, game_phase);
+    let eg_phase = 24 - mg_phase;
+
+    (mg_score * mg_phase + eg_score * eg_phase) / 24
 }
 
 fn count_psqt(board: &Board, white: &mut Score, black: &mut Score) {
