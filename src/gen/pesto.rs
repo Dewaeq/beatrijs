b90b88d39525125697bd50b244278a3312e2844c
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2023-03-25 17:14:19 +0100

    mirror table squares for white

--- src/gen/pesto.rs
+++ src/gen/pesto.rs
@@ -14,8 +14,8 @@ const fn gen_mg_pesto() -> [[Score; NUM_SQUARES]; NUM_PIECES * 2] {
     while piece < NUM_PIECES {
         let mut sq = 0;
         while sq < 64 {
-            table[piece][sq] = MG_VALUE[piece] + MG_PIECE_TABLE[piece][sq];
-            table[piece + 6][sq] = MG_VALUE[piece] + MG_PIECE_TABLE[piece][mirror(sq as Square) as usize];
+            table[piece][sq] = MG_VALUE[piece] + MG_PIECE_TABLE[piece][mirror(sq as Square) as usize];
+            table[piece + 6][sq] = MG_VALUE[piece] + MG_PIECE_TABLE[piece][sq];
 
             sq += 1;
         }
@@ -33,8 +33,8 @@ const fn gen_eg_pesto() -> [[Score; NUM_SQUARES]; NUM_PIECES * 2] {
     while piece < NUM_PIECES {
         let mut sq = 0;
         while sq < 64 {
-            table[piece][sq] = EG_VALUE[piece] + EG_PIECE_TABLE[piece][sq];
-            table[piece + 6][sq] = EG_VALUE[piece] + EG_PIECE_TABLE[piece][mirror(sq as Square) as usize];
+            table[piece][sq] = EG_VALUE[piece] + EG_PIECE_TABLE[piece][mirror(sq as Square) as usize];
+            table[piece + 6][sq] = EG_VALUE[piece] + EG_PIECE_TABLE[piece][sq];
 
             sq += 1;
         }
