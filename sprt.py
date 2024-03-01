c35f11529130b3615fad45a2525b85ddf3385b2a
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2024-03-01 17:38:03 +0100

    multiplatform sprt

--- sprt.py
+++ sprt.py
@@ -9,27 +9,32 @@ parser.add_argument("--threads", type=int, default=6, help="number of threads cu
 
 args = parser.parse_args()
 book_path = ""
+engines = ["", ""]
 
 # Compile current build
 os.system("cargo build --release")
 
-# Complile master build
-os.system("""cd tmp &&
-          git clone https://github.com/dewaeq/beatrijs &&
-          cd beatrijs &&
-          cargo build --release
-          """)
-
 if os.name == "nt":
     os.system("rmdir /s /q tmp")
     os.system("mkdir tmp")
-    book_path = 'D:/Quinten/Downloads/openings/baronbook30/baron30.bin'
-
-    os.system("copy target/release/beatrijs.exe tmp/beatrijs-new.exe")
-    os.system("copy tmp/target/release/beatrijs.exe tmp/beatrijs-master")
+    engines = ["tmp/beatrijs-new.exe", "tmp/beatrijs-master.exe"]
 else:
     os.system("rm -rf tmp")
     os.system("mkdir tmp")
+    engines = ["tmp/beatrijs-new", "tmp/beatrijs-master"]
+
+# Complile master build
+os.system("cd tmp &&"
+"git clone https://github.com/dewaeq/beatrijs &&"
+"cd beatrijs &&"
+"cargo build --release")
+
+if os.name == "nt":
+    book_path = 'D:/Quinten/Downloads/openings/baronbook30/baron30.bin'
+
+    os.system("copy target\\release\\beatrijs.exe tmp\\beatrijs-new.exe")
+    os.system("copy tmp\\beatrijs\\target\\release\\beatrijs.exe tmp\\beatrijs-master.exe")
+else:
     book_path = '/home/dewaeq/Downloads/baronbook30/baron30.bin'
 
     os.system("cp target/release/beatrijs tmp/beatrijs-new")
@@ -40,7 +45,7 @@ os.system(f"""
 cutechess-cli \
 -engine cmd=tmp/beatrijs-new name=new \
 -engine cmd=tmp/beatrijs-master name=master \
--each restart=on tc=inf/8+0.08 book='{book_path}' \
+-each restart=on tc=inf/8+0.08 book={book_path} \
 bookdepth=4 proto=uci {f"option.Hash={args.hash}" if args.hash else ""} \
 -games 2 -rounds 2500 -repeat 2 -maxmoves 200 \
 -sprt elo0=0 elo1=10 alpha=0.05 beta=0.05 \
